c60b6934ce6f8d2ab81110626de4bc34facac270
David A. Parry <david.parry@igmm.ed.ac.uk>
2016-08-22 10:10:37 +0100

    better output formatting

--- qstat_full.pl
+++ qstat_full.pl
@@ -10,10 +10,11 @@ my $job_name ;
 my %job = ();
 my @columns = qw ( job-ID  prior name user state submit/start at queue slots ja-task-ID ) ;
 my @info = qw ( job-ID state submit/start at queue slots fullname ) ; 
-print "#" . join("\t", @info) . "\n";
+my @widths = map { length($info[$_]) } 0..$#info;
+my @job_strings = ();
 foreach my $q (@qstat){
     if ($q =~ /^(\d+)/){
-        outputJobInfo();
+        getJobInfo();
         %job = ();
         @job{@columns} =  split(/\s+/, $q);
         next;
@@ -23,14 +24,34 @@ foreach my $q (@qstat){
     }
 }
 
+getJobInfo();
+
 outputJobInfo();
 
 
+##################################################
 sub outputJobInfo{
+    my $format = join(' ', map { "%-${_}s" } @widths) . "\n";
+    my $total_length = 0;
+    map { $total_length += $widths[$_] } 0..$#widths;
+    printf $format, @info;
+    print "-" x $total_length . "-" x @info . "\n";
+    foreach my $j (@job_strings){
+        { 
+            no warnings 'uninitialized';
+            printf $format, @$j;
+        }
+    }
+}
+
+##################################################
+sub getJobInfo{
     return if not %job;
     { 
         no warnings 'uninitialized';
         my @output = map { $job{$_} } @info ; 
-        print join("\t", @output) . "\n";
+        @widths    = map { length($output[$_]) > $widths[$_] ? length($output[$_]) : $widths[$_] } 0..$#info;
+        #print join("\t", @output) . "\n";
+        push @job_strings, \@output;
     }
 }
