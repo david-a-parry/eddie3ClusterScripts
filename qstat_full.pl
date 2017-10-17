81a8f71ee69a4833a88c9cab4c39a81fd0f19016
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-10-17 17:52:33 +0100

    add options to show only queued/held/errored jobs

--- qstat_full.pl
+++ qstat_full.pl
@@ -4,45 +4,67 @@ use strict;
 use warnings; 
 use Getopt::Long;
 
-my %opts = ();
+my %opts = (d => 30);
 GetOptions
 (
     \%opts,
     'h|help',
     'r|running',
+    'q|queued',
+    'i|held',
+    'e|errored',
+    'w|wait_id=i',
+    'd|delay=i',
 ) or usage("syntax error!");
 usage() if $opts{h};
 
 
-my @qstat = split("\n", `qstat -r`); 
-exit if not @qstat;
 my $job_no ;
 my $job_name ;
 my %job = ();
 my @columns = ( "job-ID",  "prior", "name", "user", "state", "submit/start at", "queue", "slots", "ja-task-ID", ) ;
+my %indices = ();
 
 #cols are different lengths separated by spaces
 #in order to detect empty cols we need to know where to look
-my %indices = getColIndices($qstat[0]);
 
 my @info = ("job-ID", "prior", "name", "user", "state", "submit/start at", "queue", "slots", "fullname" ) ;
 my @widths = map { length($info[$_]) } 0..$#info;
+my %current_jobs = (); 
 my @job_strings = ();
-foreach my $q (@qstat){
-    if ($q =~ /^\s*(\d+)/){
-        getJobInfo();
-        %job = ();
-        getColumnValues($q);
-        next;
-    }
-    if ($q =~ /^\s+Full jobname:\s+(\S+)/){
-        $job{fullname} = $1;
-    }
-}
+my @qstat = split("\n", `qstat -r`); 
+processQstat();
 
 getJobInfo();
 
 outputJobInfo();
+
+if ($opts{w}){
+    while (exists $current_jobs{$opts{w}}){
+        sleep($opts{d});
+        @qstat = split("\n", `qstat -r`); 
+        processQstat();
+        getJobInfo();
+        outputJobInfo();
+    }
+}
+
+##################################################
+sub processQstat{   
+    exit if not @qstat;
+    %indices = getColIndices($qstat[0]);
+    foreach my $q (@qstat){
+        if ($q =~ /^\s*(\d+)/){
+            getJobInfo();
+            %job = ();
+            getColumnValues($q);
+            next;
+        }
+        if ($q =~ /^\s+Full jobname:\s+(\S+)/){
+            $job{fullname} = $1;
+        }
+    }
+}
 
 ##################################################
 sub getColumnValues{
@@ -76,7 +98,15 @@ sub outputJobInfo{
     printf $format, @info;
     print "-" x $total_length . "-" x $#info . "\n";
     foreach my $j (@job_strings){
-        if (not $opts{r} or ( $opts{r} and $j->[4] =~ /r/) ){
+        if ($opts{r} or $opts{q} or $opts{i} or $opts{e}){
+            if ( ($opts{r} and $j->[4] =~ /r/) or
+                 ($opts{q} and $j->[4] =~ /^\s*qw\s*$/) or 
+                 ($opts{i} and $j->[4] =~ /^\s*hqw\s*$/) or 
+                 ($opts{e} and $j->[4] =~ /E/ )
+            ){
+                printf $format, @$j;
+            }
+        }else{
             printf $format, @$j;
         }
     }
@@ -89,6 +119,8 @@ sub getJobInfo{
     @widths    = map { length($output[$_]) > $widths[$_] ? length($output[$_]) : $widths[$_] } 0..$#info;
     #print join("\t", @output) . "\n";
     push @job_strings, \@output;
+    (my $j = $job{'job-ID'}) =~ s/\s//g;
+    $current_jobs{$j} = \%job;
 }
 
 
@@ -104,10 +136,23 @@ sub usage{
 
         -h,--help
             Show this message and exit
+
         -r,--running
             Show only jobs that are running
 
-    
+        -q,--queued
+            Show only queued waiting jobs (qw)
+
+        -i,--held
+            Show only held jobs (hqw)
+
+        -e,--errored
+            Show only jobs in error state
+
+        -w job-ID --wait_id job-ID
+            Continue running while the given job-ID is running. The numeric
+            job-ID must be given, not the job name.
+
 EOT
     ;
     exit 1 if $msg;
