49e006695a9cd85ed7317dd15a84ea3aed303ff3
David A. Parry <david.parry@igmm.ed.ac.uk>
2018-01-17 14:23:38 +0000

    improve documentation; add --wait_ids option

--- queue_scripts.pl
+++ queue_scripts.pl
@@ -2,22 +2,27 @@
 use strict;
 use warnings;
 use Getopt::Long;
+my @pre_wait = ();
 my %opts = (p => 1);
 GetOptions(
     \%opts,
     "p|processes=i",
     "d|dummy",
+    "w|wait_ids=s",
     "h|help",
 ) or usage("Error in options");
 usage() if $opts{h};
 usage("At least two scripts are required") if @ARGV < 2;
 my $n = 0;
+die "--processes option must be greater than 0\n" if $opts{p} < 1;
 my @holds = ();
 while (my $script = shift){
     my $cmd = "qsub ";
     my $hidx = $n - $opts{p};
     if (@holds > $hidx and $hidx >= 0){
         $cmd .= "-hold_jid $holds[$n - $opts{p}] ";
+    }elsif ($opts{w}){
+        $cmd .= "-hold_jid $opts{w} ";
     }
     $cmd .= $script;
     print STDERR "Executing: $cmd\n";
@@ -29,7 +34,13 @@ while (my $script = shift){
         if ($output =~ /Your job (\d+) .* has been submitted/){
             push @holds, $1;
         }else{
-            die "Error parsing qsub output for $cmd!\nOutput was: $output";
+            my $msg = "Error parsing qsub output for $cmd!\n".
+                      "Output was: $output\n";
+            if (@holds){
+                $msg .= "You may wish to terminate the previously submitted ".
+                        "jobs. IDs of these jobs are: ".join(",". @holds)."\n";
+            }
+            die $msg;
         }
     }
     $n++;
@@ -56,13 +67,24 @@ sub usage{
 
 Usage: $0 [options] script1.sh script2.sh [...scriptN.sh]
 
+Scripts will be submitted to the queue in the order provided on the 
+commandline. By default each script will wait for the previous script to exit 
+before starting, but several scripts can be run simulataneously using the 
+--processes option (see below). 
+
 Options:
     
     -p INT --processes INT
-        Number of scripts to run in at simultaneously
+        Number of scripts to run simultaneously. That is, if set to 2, script N 
+        will wait for script N-2 to complete before starting. Default=1 (i.e. 
+        scripts all run sequentially).
 
     -d, --dummy
-        Do a dummy run without  actually submitting scripts
+        Do a dummy run without actually submitting scripts.
+
+    -w job-IDS --wait_ids job-IDs
+        One or more job IDs to wait for before executing ANY script, separated
+        with commas.
 
     -h, --help
         Show this message and exit.
